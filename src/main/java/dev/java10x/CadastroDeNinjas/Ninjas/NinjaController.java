2449db5ddfe955f9f2cfb47249d4bfd9fc1d634c
Paulo Nadson de Sousa Bandeira <paulo@nadson.nadson.com>
2026-09-01 19:07:58 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,17 +1,29 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class NinjaController {
 
     @GetMapping("/boasVindas")
-    public String boasVindas(){
+    String boasVindas(){
         return "Essa é a minha primeira mensagem por rota :>";
     }
 
+    @PostMapping("/criar")
+    String criarNinja(){ return "Ninja criado";}
+
+    @GetMapping("/todos")
+    String mostrarNinjas(){ return "Todos os ninjas";}
+
+    @GetMapping("/MostrarNinjaPorID")
+    String ninjaID(){return "Ninja ID";}
+
+    @PutMapping("AlterarNinjaID")
+    String alterarNinja(){return "Ninja editado";}
+
+    @DeleteMapping
+    String deletarPorID(){return "Ninja deletado";}
 
 }

0481b978bb06721cb82768ddfa1e740fd0154708
Paulo Nadson de Sousa Bandeira <paulo@nadson.nadson.com>
2026-09-07 16:06:05 -0300

    fix: Tira a responsabilidade do model e passa para o DTO usando streams no mapper

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -22,19 +22,19 @@ public class NinjaController {
 
     // GET -> Listar todos os ninjas
     @GetMapping("/listar")
-    List<NinjaModel> listarNinjas() {
+    List<NinjaDTO> listarNinjas() {
         return ninjaService.listarNinjas();
     }
 
     // GET -> Buscar ninja pelo ID
     @GetMapping("/listar/{id}")
-    public NinjaModel listarNinjasPorId(@PathVariable Long id) {
+    public NinjaDTO listarNinjasPorId(@PathVariable Long id) {
         return ninjaService.listarNinjasPorID(id);
     }
 
     // PUT -> Alterar ninja pelo ID
     @PutMapping("/alterar/{id}")
-    NinjaModel alterarNinja(@PathVariable Long id,@RequestBody NinjaModel ninjaAtualizado) {
+    NinjaDTO alterarNinja(@PathVariable Long id,@RequestBody NinjaDTO ninjaAtualizado) {
         return ninjaService.atualizarNinja(id, ninjaAtualizado);
     }
 
